feea764c25cc754563401d7127d9a2df5ed3a8c7
ko-chan2312 <59499167+ko-chan2312@users.noreply.github.com>
2020-01-04 23:35:49 +0900

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,10 @@
+$(function(){
+ $('.index').click(function(){
+   $('.active').removeClass('active');
+   var clickedIndex = $('.index').index($(this));
+   $('.language-item').eq(clickedIndex).addClass('active');
+
+  
+ 
+});
+});
