eb42b2ebd723eac9c90b3ddcc88a91d8fff0d12b
ko-chan2312 <59499167+ko-chan2312@users.noreply.github.com>
2020-01-05 23:13:48 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -3,6 +3,9 @@ $(function(){
    $('.active').removeClass('active');
    var clickedIndex = $('.index').index($(this));
    $('.language-item').eq(clickedIndex).addClass('active');
+   $('.menu-icon').click(function(){
+     $('.menu-list').show();
+   })
 
   
  
