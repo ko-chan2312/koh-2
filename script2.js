d0f528982fadcfa7e0f9fed6058a657bc3230cb4
ko-chan2312 <59499167+ko-chan2312@users.noreply.github.com>
2020-02-03 22:47:34 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,4 +1,19 @@
 $(function(){
+  $('.menu-click').click(function(){
+    var $list =$(this).find('.menu-list');
+    if($list.hasClass('open')){
+      $list.removeClass('open');
+      $list.slideUp();
+    }else{
+      $list.addClass('open');
+      $list.slideDown();
+    }
+  })
+})
+
+
+
+$(function(){
  $('.index').click(function(){
    $('.active').removeClass('active');
    var clickedIndex = $('.index').index($(this));
